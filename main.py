80581804e9a4e40e97d0b38c2c0ad1d9870a750a
Severin Rohrer <severin.rohrer@students.ffhs.ch>
2023-11-13 17:52:57 +0100

    Show inputs and execution value

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Das Hauptformular wird angezeigt
+    # Das Hauptformular wird angezeigt.
     return render_template('score_calculator.html')
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    # Eingaben aus dem Formular werden abgerufen
+    # Eingaben aus dem Formular werden abgerufen.
     difficulty = float(request.form['difficulty'])
     deductions = float(request.form['deductions'])
 
-    # Berechnung der Endnote
-    final_score = difficulty + 10 - deductions
+    # Berechnung der Ausführungsnote.
+    execution = 10 - deductions
 
-    # Ergebnis wird zurück an das HTML-Formular gesendet
-    return render_template('score_calculator.html', final_score=final_score)
+    # Berechnung der Endnote.
+    final_score = difficulty + execution
+
+    # Ergebnis wird zurück an das HTML-Formular gesendet.
+    return render_template('score_calculator.html', difficulty=difficulty, execution=execution, final_score=final_score)
 
 if __name__ == '__main__':
     app.run(debug=True)
