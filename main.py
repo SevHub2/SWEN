c8e6e8bc051ba11f7afca7d17b9ace5e598f6d4a
Severin Rohrer <severin.rohrer@students.ffhs.ch>
2023-11-13 12:49:54 +0100

    start

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-"""
-This is the suggested main entry Python source file for your project
-"""
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    print("Hello")
+@app.route('/')
+def index():
+    # Das Hauptformular wird angezeigt
+    return render_template('score_calculator.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    # Eingaben aus dem Formular werden abgerufen
+    difficulty = float(request.form['difficulty'])
+    deductions = float(request.form['deductions'])
+
+    # Berechnung der Endnote
+    final_score = difficulty + 10 - deductions
+
+    # Ergebnis wird zurück an das HTML-Formular gesendet
+    return render_template('score_calculator.html', final_score=final_score)
+
+if __name__ == '__main__':
+    app.run(debug=True)
