63d15fa2976de76d7001328a08df7fe8fcf81894
Severin Rohrer <severin.rohrer@students.ffhs.ch>
2023-12-16 12:48:44 +0100

    CleanCode, Begrenzung Eingabe Abzüge 10, Layout

--- main.py
+++ main.py
@@ -2,28 +2,34 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Grundkonstanten
+MAX_SCORE = 10
+
 @app.route('/')
 def index():
-    # Das Hauptformular wird angezeigt.
+    """ Zeigt das Hauptformular an. """
     return render_template('score_calculator.html')
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    # Eingaben aus dem Formular werden abgerufen.
-    difficulty = float(request.form['difficulty'])
-    deductions = float(request.form['deductions'])
+    """ Berechnet die Endnote basierend auf den Formulareingaben. """
+    try:
+        difficulty = float(request.form['difficulty'])
+        deductions = float(request.form['deductions'])
 
-    # Berechnung der Ausführungsnote.
-    execution = 10 - deductions
+        # Berechnung der Ausführungsnote
+        execution = MAX_SCORE - deductions
 
-    # Berechnung der Endnote.
-    final_score = difficulty + execution
+        # Berechnung der Endnote
+        final_score = difficulty + execution
 
-    # Ergebnis wird zurück an das HTML-Formular gesendet.
-    return render_template('score_calculator.html', difficulty=difficulty, execution=execution, final_score=final_score)
+        return render_template('score_calculator.html', 
+                               difficulty=difficulty, 
+                               execution=execution, 
+                               final_score=final_score)
+    except ValueError:
+        # Fehlerbehandlung für ungültige Eingaben
+        return "Ungültige Eingabe. Bitte geben Sie gültige Zahlen ein.", 400
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
